9cbcb454bc83237f0fed96505e67eb35c702ffbc
Nikith Kumar Bandaru <nikithkumarbandaru007@gmail.com>
2023-11-26 16:30:28 -0600

    protecting endpoints

--- routes/elephants.js
+++ routes/elephants.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const passport = require('passport');
 const elephant_controlers= require('../controllers/elephants');
 var router = express.Router();
 /* GET elephants */
@@ -10,7 +11,22 @@ router.get('/elephants/:id', elephant_controlers.elephant_detail);
 router.get('/detail', elephant_controlers.elephant_view_one_Page);
 /* GET create costume page */
 router.get('/create', elephant_controlers.elephant_create_Page);
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 /* GET create update page */
-router.get('/update', elephant_controlers.elephant_update_Page);
+router.get('/update', secured, elephant_controlers.elephant_update_Page);
 /* GET delete costume page */
 router.get('/delete', elephant_controlers.elephant_delete_Page);
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
