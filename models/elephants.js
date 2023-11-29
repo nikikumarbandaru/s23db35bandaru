3a0bfc931fe545b03ec7e5acc1047ed103f811ee
Nikith Kumar Bandaru <nikithkumarbandaru007@gmail.com>
2023-11-28 23:19:01 -0600

    authentication

--- models/elephants.js
+++ models/elephants.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose")
 const elephantSchema = mongoose.Schema({
-elephant_Habitat: String,
+elephant_Habitat:
+{
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 14,
+},
 elephant_Weight: Number,
 elephant_Lifespan: String,
 elephant_TuskLength: Number
